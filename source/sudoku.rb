61f6657174582131001fb0c82a8b9b6391a2dd51
Alicia <email": null>
2015-09-11 14:29:44 -0400

    Unfinished solution; skeleton of project

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,11 +1,40 @@
 class Sudoku
   def initialize(board_string)
+    @board = board_string
+    @possibilities = (1..9).to_a
   end
 
   def solve
+    until solved?
+      @board.each_with_index do |char, idx|
+        if empty?(char)
+          solve_char(idx)
+        end
+      end
+    end
+  end
+
+  def empty?(char)
+    char == "-"
+  end
+
+  def solved?
   end
 
   def board
+  end
+
+  def solve_char(idx)
+  end
+
+  def check_box(idx)
+  end
+
+  def check_col(idx)
+
+  end
+
+  def check_row(idx)
   end
 
   # Returns a string representing the current state of the board
