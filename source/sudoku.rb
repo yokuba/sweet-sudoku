d082f668842af2308fefc7990d6aa5d933bd7a67
Alicia <email": null>
2015-09-11 12:02:49 -0400

    Initialize with source files

---
+++ source/sudoku.rb
@@ -0,0 +1,14 @@
+class Sudoku
+  def initialize(board_string)
+  end
+
+  def solve
+  end
+
+  def board
+  end
+
+  # Returns a string representing the current state of the board
+  def to_s
+  end
+end
