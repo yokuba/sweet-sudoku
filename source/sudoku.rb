53b739c37cc339bae8572cdb8e751ed21e9d0c12
Alicia <email": null>
2015-09-11 17:33:19 -0400

    End of day save - partial solution

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,7 +1,6 @@
 class Sudoku
   def initialize(board_string)
-    @board = board_string
-    @possibilities = (1..9).to_a
+    @board = board_string.chars
   end
 
   def solve
@@ -9,6 +8,7 @@ class Sudoku
       @board.each_with_index do |char, idx|
         if empty?(char)
           solve_char(idx)
+          # validate?
         end
       end
     end
@@ -19,25 +19,90 @@ class Sudoku
   end
 
   def solved?
+    !@board.include?("-")
   end
 
-  def board
+  # def board
+  # end
+
+  def validate?(char, group)
+    group.count(char) == 1
   end
 
   def solve_char(idx)
+    possible = (1..9).to_a
+    not_possible = [create_box(idx), create_col(idx), create_row(idx)].flatten
+
+    possible -= not_possible
+    @board[idx] = possible[0]
   end
 
-  def check_box(idx)
+  def create_box(position)
+    blockIndex = calculate_box_index(position)
+
+    box_members = []
+
+    while (box_members.length < 9)
+      box_row = @board[blockIndex, 3]
+      box_members.concat(box_row)
+      blockIndex += 9
+    end
+
+    box_members
+
   end
 
-  def check_col(idx)
+  def calculate_box_index(position)
+    row = position / 9
+    col = position % 9
 
+    blockRow = (row / 3) * 3
+    blockCol = (col / 3) * 3
+
+    (blockRow * 9) + blockCol
   end
 
-  def check_row(idx)
+  def create_col(position)
+    col_idx = position % 9
+    col_members = []
+
+    cols = []
+
+    @board.each_slice(9) do |slice|
+      cols << slice
+    end
+
+    cols = cols.transpose
+
+    cols.each_with_index do |char, index|
+      if (index % 9) == col_idx
+        col_members << char
+      end
+    end
+
+    col_members
+  end
+
+  def create_row(position)
+    row_idx = position / 9
+
+    row_members = []
+
+    @board.each_with_index do |char, index|
+      if (index / 9) == row_idx
+        row_members << char
+      end
+    end
+
+    row_members
   end
 
   # Returns a string representing the current state of the board
   def to_s
+    output = ''
+    @board.each_slice(9) do |slice|
+      output << slice.join('   ') + "\n\n"
+    end
+    output
   end
 end
